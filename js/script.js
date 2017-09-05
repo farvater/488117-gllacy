a56f6ee405f817a3babb5a65747c26aa6d9a2131
Игуменова Наталья Л <natalya.igumenova@corp.sokolov.ru>
2017-09-05 17:43:29 +0300

    Подгон стилей index

--- js/script.js
+++ js/script.js
@@ -1,5 +1,33 @@
-$(document).ready(function(){
-	$(document).on('mouseover','.header-navigation-item a',function(){
-		$(this).addClass('open');
-	})
-})
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [59.938631, 30.323055],
+            zoom: 9
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Собственный значок метки',
+            balloonContent: 'Это красивая метка'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: 'img/map-ice-cream.png',
+            // Размеры метки.
+            iconImageSize: [30, 42],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-5, -38]
+        });
+
+    myMap.geoObjects
+        .add(myPlacemark)
+});
+
