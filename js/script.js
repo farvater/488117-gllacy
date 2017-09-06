0b3827698528f1c0eac6c630cf3c839f8709d88c
Игуменова Наталья Л <natalya.igumenova@corp.sokolov.ru>
2017-09-06 17:48:02 +0300

    Подгон стилей catalog

--- js/script.js
+++ js/script.js
@@ -1,33 +1,31 @@
-ymaps.ready(function () {
-    var myMap = new ymaps.Map('map', {
-            center: [59.938631, 30.323055],
-            zoom: 9
-        }, {
-            searchControlProvider: 'yandex#search'
-        }),
-
-        // Создаём макет содержимого.
-        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
-        ),
-
-        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-            hintContent: 'Собственный значок метки',
-            balloonContent: 'Это красивая метка'
-        }, {
-            // Опции.
-            // Необходимо указать данный тип макета.
-            iconLayout: 'default#image',
-            // Своё изображение иконки метки.
-            iconImageHref: 'img/map-ice-cream.png',
-            // Размеры метки.
-            iconImageSize: [30, 42],
-            // Смещение левого верхнего угла иконки относительно
-            // её "ножки" (точки привязки).
-            iconImageOffset: [-5, -38]
+        var link = document.querySelector(".popup");
+        var popup = document.querySelector(".modal-login");
+        var close = popup.querySelector(".modal-close");
+        var form = popup.querySelector("form");
+        var login = popup.querySelector("[name=name]");
+        var password = popup.querySelector("[name=email]");
+        link.addEventListener("click", function (evt) {
+            evt.preventDefault();
+            popup.classList.add("modal-show");
+            login.focus();
         });
-
-    myMap.geoObjects
-        .add(myPlacemark)
-});
+        close.addEventListener("click", function (evt) {
+            evt.preventDefault();
+            popup.classList.remove("modal-show");
+        });
+        form.addEventListener("submit", function (evt) {
+            if (!login.value || !password.value) {
+                evt.preventDefault();
+            console.log("Нужно ввести логин и пароль");
+            } else {
+                localStorage.setItem("login", login.value);
+            }
+        });
+        window.addEventListener("keydown", function (evt) {
+            if (evt.keyCode === 27) {
+                if (popup.classList.contains("modal-show")) {
+                    popup.classList.remove("modal-show");
+                }
+            }
+        });
 
