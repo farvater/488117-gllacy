129b0c025da1e60164929272ef045f1bb4b31fac
Keks <keks@htmlacademy.ru>
2017-09-10 19:31:45 +0300

    :heavy_check_mark: Сборка #6

--- 6/js/script.js
+++ 6/js/script.js
@@ -1,5 +1,5 @@
-        var link = document.querySelector(".popup");
-        var popup = document.querySelector(".modal-login");
+        var popup = document.querySelector(".popup");
+        var link = document.querySelector(".modal-login");
         var close = popup.querySelector(".modal-close");
         var form = popup.querySelector("form");
         var login = popup.querySelector("[name=name]");
@@ -12,14 +12,6 @@
         close.addEventListener("click", function (evt) {
             evt.preventDefault();
             popup.classList.remove("modal-show");
-        });
-        form.addEventListener("submit", function (evt) {
-            if (!login.value || !password.value) {
-                evt.preventDefault();
-            console.log("Нужно ввести логин и пароль");
-            } else {
-                localStorage.setItem("login", login.value);
-            }
         });
         window.addEventListener("keydown", function (evt) {
             if (evt.keyCode === 27) {
